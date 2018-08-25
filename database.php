8f7ebe2b3c51beec9e2eedfcf8088f98d5b67398
Danish Malik <malik083@morris.umn.edu>
2018-08-25 14:03:17 +0000

    added db file

---
+++ database.php
@@ -0,0 +1,24 @@
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn' => '',
+    'hostname' => 'us-cdbr-iron-east-01.cleardb.net',
+    'username' => 'bf1920e1864032',
+    'password' => '901617e5',
+    'database' => 'heroku_1961c11779a1f40',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char-set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+    );
